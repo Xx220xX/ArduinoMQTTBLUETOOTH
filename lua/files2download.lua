9838c6ade042ccd157ad50f38f239ee7386cfbf5
Henrique santos de Lima <hslhenriquegoscara@gmail.com>
2020-03-03 17:42:12 -0300

    Update files2download.lua

--- lua/files2download.lua
+++ lua/files2download.lua
@@ -1,9 +1,12 @@
 -- arquivos obb em codigo lua para download
-name = {'alarme','aquecer','blink','on','off','onFor','refrigerar',__version = 1003,__version_Name = 'beta 1.003',linkFormat = 'https://raw.githubusercontent.com/Xx220xX/ArduinoMQTTBLUETOOTH/master/lua/%s.lua'}
+name = {'alarme','aquecer','blink','on','off','onFor','refrigerar',__version = 1004,__version_Name = 'beta 1.004',linkFormat = 'https://raw.githubusercontent.com/Xx220xX/ArduinoMQTTBLUETOOTH/master/lua/%s.lua'}
 --[[
 	Nota: 
+	Versao beta 1.004 -1.003
+	*  fix utf-8 bug cause
+		
 
-	Versao beta 1.001
+	Versao beta 1.002
 	* Modificado divisao por multiplicacao
 		um pequeno erro na conversao de segundos para segundos.
 	_____________
