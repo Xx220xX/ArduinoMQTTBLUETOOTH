5739d26a8c6e14f8ecc9cff65166c0f396a58953
Henrique santos de Lima <hslhenriquegoscara@gmail.com>
2020-03-03 13:27:18 -0300

    Update files2download.lua

--- lua/files2download.lua
+++ lua/files2download.lua
@@ -1,1 +1,4 @@
-files.add('aquece',link)
+-- arquivos obb em codigo lua para download
+name = {__version = 1001,__version_Name = 'beta 1.001','alarme','aquecer','blink',
+        'on','off','onFor','refrigerar',linkFormat = 'https://raw.githubusercontent.com/Xx220xX/ArduinoMQTTBLUETOOTH/master/lua/%s.lua'}
+return name
