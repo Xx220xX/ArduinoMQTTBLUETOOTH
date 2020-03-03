6102c223614de6e1c9e701ca7eaef12026fcc77a
Henrique santos de Lima <hslhenriquegoscara@gmail.com>
2020-03-03 17:40:53 -0300

    fix utf-8 bug cause

--- lua/alarme.lua
+++ lua/alarme.lua
@@ -11,8 +11,11 @@ c.args[1].list['Tomada 4']=3
 c.args[2] =  Args.new('Hora','int',[[]],'tempo invalido',function (t) return t>=0 and t<24 end)
 c.args[3] =  Args.new('Minuto','int',[[]],'tempo invalido',function (t) return t>=0 and t<59 end)
 c.args[4] =  Args.new('Segundo','int',[[]],'tempo invalido',function (t) return t>=0 and t<59 end)
+
 c.args[5] =  Args.new('Dias da semana','dropDown',[[Dias da semana que para repetir alarme]])
-c.args[5].list ={Domingo=1,Segunda=2,Terça=3,Quarta=4,Quinta=5,Sexta=6,Sábado=7}
+c.args[5].list ={Domingo=1,Segunda=2,Quarta=4,Quinta=5,Sexta=6}
+c.args[5].list['Terça']= 3
+c.args[5].list['Sábado']= 7
 
 c.args[6] =  Args.new('Periodo','float',[[refere-se ao tempo em segundos que permanecera ligada]],'tempo invalido',function (t) return t>=0 end)
 c.args[6].getValue = function (val) return val*1000 end
